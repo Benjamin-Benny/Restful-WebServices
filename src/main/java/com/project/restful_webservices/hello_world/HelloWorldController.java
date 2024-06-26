01b9e5e79a3ea64be1861706973c3d8ab349ff0d
Benjamin Benny <35957758+Benjamin-Benny@users.noreply.github.com>
2024-06-26 07:57:32 +0530

    completed get mapping

--- src/main/java/com/project/restful_webservices/hello_world/HelloWorldController.java
+++ src/main/java/com/project/restful_webservices/hello_world/HelloWorldController.java
@@ -1,4 +1,15 @@
 package com.project.restful_webservices.hello_world;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String HelloWorld() {
+        return "Hello World";
+    }
 }
